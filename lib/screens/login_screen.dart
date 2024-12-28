115e9080a6723933935debbaea579e235a8eb94b
MuhammadRifqiF <mrifqifrds@gmail.com>
2024-12-28 19:26:38 +0700

    perbaikan desain

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -13,32 +13,15 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
   // Fungsi untuk login
   Future<void> _login() async {
-    try {
-      if (_emailController.text.isEmpty || _passwordController.text.isEmpty) {
-        // Validasi jika email atau password kosong
-        showDialog(
-          context: context,
-          builder: (context) {
-            return AlertDialog(
-              title: Text("Error"),
-              content: Text("Please enter both email and password."),
-              actions: [
-                TextButton(
-                  onPressed: () {
-                    Navigator.of(context).pop();
-                  },
-                  child: Text('OK'),
-                ),
-              ],
-            );
-          },
-        );
-        return;
-      }
+    if (!_formKey.currentState!.validate()) {
+      return; // Hentikan jika validasi gagal
+    }
 
+    try {
       // Melakukan login menggunakan FirebaseAuth
       await FirebaseAuth.instance.signInWithEmailAndPassword(
         email: _emailController.text,
@@ -88,38 +71,164 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Login'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            TextField(
-              controller: _emailController,
-              decoration: InputDecoration(labelText: 'Email'),
-              keyboardType: TextInputType.emailAddress,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Form(
+            key: _formKey, // Menambahkan Form widget untuk validasi
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                // Menambahkan barisan untuk tombol back
+                Row(
+                  children: [
+                    // Tombol back
+                    IconButton(
+                      icon: Icon(Icons.arrow_back, color: Colors.black),
+                      onPressed: () {
+                        Navigator.pop(
+                            context); // Navigasi kembali ke layar sebelumnya
+                      },
+                    ),
+                  ],
+                ),
+                SizedBox(height: 30), // Menambahkan jarak sebelum teks "Login"
+
+                // Teks "Login"
+                Text(
+                  'Login',
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                    color: Color.fromRGBO(107, 33, 168, 1),
+                  ),
+                ),
+                SizedBox(height: 10),
+
+                // Teks "Welcome back! Please login to your account"
+                Text(
+                  'Welcome back! Please login to your account',
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.grey[600],
+                  ),
+                ),
+                SizedBox(
+                    height: 0), // Jarak antara teks "Welcome back" dan gambar
+
+                // Menambahkan gambar login di bawah teks "Welcome back"
+                Center(
+                  child: Image.asset(
+                    'assets/images/login1.png',
+                    height: 300, // Menyesuaikan ukuran gambar sesuai keinginan
+                    width: MediaQuery.of(context)
+                        .size
+                        .width, // Lebar gambar menyesuaikan layar
+                    fit: BoxFit
+                        .cover, // Menyesuaikan gambar dengan rasio yang benar tanpa distorsi
+                  ),
+                ),
+
+                SizedBox(height: 10), // Jarak antara gambar dan form input
+
+                // Email TextFormField
+                TextFormField(
+                  controller: _emailController,
+                  decoration: InputDecoration(
+                    labelText: 'Email',
+                    labelStyle:
+                        TextStyle(color: Color.fromRGBO(107, 33, 168, 1)),
+                    border: OutlineInputBorder(),
+                    hintText: 'Enter your email',
+                    contentPadding:
+                        EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+                  ),
+                  keyboardType: TextInputType.emailAddress,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter your email';
+                    }
+                    // Cek format email
+                    if (!RegExp(
+                            r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$")
+                        .hasMatch(value)) {
+                      return 'Please enter a valid email';
+                    }
+                    return null;
+                  },
+                ),
+                SizedBox(height: 20),
+
+                // Password TextFormField
+                TextFormField(
+                  controller: _passwordController,
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    labelText: 'Password',
+                    labelStyle:
+                        TextStyle(color: Color.fromRGBO(107, 33, 168, 1)),
+                    border: OutlineInputBorder(),
+                    hintText: 'Enter your password',
+                    contentPadding:
+                        EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter your password';
+                    }
+                    return null;
+                  },
+                ),
+                SizedBox(height: 30),
+
+                // Login Button
+                ElevatedButton(
+                  onPressed: _login,
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color.fromRGBO(107, 33, 168, 1),
+                    padding: EdgeInsets.symmetric(vertical: 15),
+                    minimumSize: Size(double.infinity, 50),
+                  ),
+                  child: Text(
+                    'Login',
+                    style: TextStyle(color: Colors.white, fontSize: 16),
+                  ),
+                ),
+                SizedBox(height: 10),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment
+                      .center, // Menyelaraskan secara horizontal di tengah
+                  children: [
+                    Text(
+                      'Belum punya akun? ',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontFamily: 'Roboto',
+                        fontSize: 16,
+                      ),
+                    ),
+                    // Signup Button
+                    TextButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (_) => const SignupScreen()),
+                        );
+                      },
+                      child: Text(
+                        'Daftar',
+                        style: TextStyle(
+                          color: Color.fromRGBO(107, 33, 168, 1),
+                          fontSize: 16, // Disesuaikan agar ukuran font seragam
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
             ),
-            TextField(
-              controller: _passwordController,
-              obscureText: true,
-              decoration: InputDecoration(labelText: 'Password'),
-            ),
-            ElevatedButton(
-              onPressed: _login, // Panggil fungsi login
-              child: Text('Login'),
-            ),
-            TextButton(
-              onPressed: () {
-                // Navigasi ke halaman signup jika belum memiliki akun
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (_) => const SignupScreen()),
-                );
-              },
-              child: Text('Belum punya akun? Daftar'),
-            ),
-          ],
+          ),
         ),
       ),
     );
