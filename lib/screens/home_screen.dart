8a0eb2f0ecda9b2db3e05055829e7c21f54a020a
AdzillatunnisaM <zaleaa2412@gmail.com>
2024-12-28 19:36:50 +0700

    perbaikan tata letak dan desain

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -4,6 +4,10 @@ import 'login_screen.dart';
 import 'task_screen.dart';
 import 'calendar_screen.dart';
 import 'account_screen.dart';
+import 'package:provider/provider.dart';
+import 'package:todolist/providers/task_provider.dart';
+import 'dart:convert';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -15,11 +19,42 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   int _selectedIndex = 0;
 
+  Future<String?> _getProfileImageBase64() async {
+    final user = FirebaseAuth.instance.currentUser;
+    if (user != null) {
+      final userDoc = await FirebaseFirestore.instance
+          .collection('users')
+          .doc(user.uid)
+          .get();
+      return userDoc.exists ? userDoc['photoURL'] : null;
+    }
+    return null;
+  }
+
   final List<Widget> _pages = [
     TaskScreen(),
     const CalendarScreen(),
     AccountScreen(),
   ];
+  final TextEditingController _taskController = TextEditingController();
+  DateTime _selectedDate = DateTime.now();
+
+  String _userEmail = '';
+
+  @override
+  void initState() {
+    super.initState();
+    _getUserEmail();
+  }
+
+  void _getUserEmail() {
+    final user = FirebaseAuth.instance.currentUser;
+    if (user != null) {
+      setState(() {
+        _userEmail = user.email ?? 'Unknown User';
+      });
+    }
+  }
 
   void _onItemTapped(int index) {
     setState(() {
@@ -35,30 +70,229 @@ class _HomeScreenState extends State<HomeScreen> {
     );
   }
 
+  Future<void> _showAddTaskDialog(BuildContext context) async {
+    final TaskProvider taskProvider =
+        Provider.of<TaskProvider>(context, listen: false);
+    return showDialog<void>(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20),
+            ),
+            title: const Text('Add Task'),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                TextField(
+                  controller: _taskController,
+                  decoration: InputDecoration(
+                    labelText: 'Task Name',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 10),
+                ListTile(
+                  title: Text('Task Date: ${_selectedDate.toLocal()}'),
+                  trailing: const Icon(Icons.calendar_today),
+                  onTap: () async {
+                    final selectedDate = await showDatePicker(
+                      context: context,
+                      initialDate: _selectedDate,
+                      firstDate: DateTime(2000),
+                      lastDate: DateTime(2101),
+                    );
+                    if (selectedDate != null && selectedDate != _selectedDate) {
+                      setState(() {
+                        _selectedDate = selectedDate;
+                      });
+                    }
+                  },
+                ),
+              ],
+            ),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: const Text('Cancel'),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  if (_taskController.text.isNotEmpty) {
+                    taskProvider.addTask(_taskController.text, _selectedDate,
+                        taskProvider.selectedCategory);
+                    _taskController.clear();
+                    Navigator.pop(context);
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(content: Text('Task added successfully')),
+                    );
+                  }
+                },
+                child: const Text('Add Task'),
+              ),
+            ],
+          );
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('My ToDo List'),
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.logout),
-            onPressed: _logout,
-            tooltip: 'Logout',
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(80),
+        child: AppBar(
+          flexibleSpace: Container(
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                colors: [Color.fromRGBO(107, 33, 168, 1), Colors.deepPurple],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+              ),
+            ),
           ),
-        ],
+          elevation: 0,
+          title: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: Row(
+              children: [
+                FutureBuilder<String?>(
+                  future: _getProfileImageBase64(),
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return const CircleAvatar(
+                        radius: 20,
+                        child: CircularProgressIndicator(),
+                      );
+                    } else if (snapshot.hasError) {
+                      return const CircleAvatar(
+                        radius: 20,
+                        child: Icon(Icons.error),
+                      );
+                    } else {
+                      String? base64Image = snapshot.data;
+                      if (base64Image != null && base64Image.isNotEmpty) {
+                        return CircleAvatar(
+                          radius: 20,
+                          backgroundImage:
+                              MemoryImage(base64Decode(base64Image)),
+                        );
+                      } else {
+                        return const CircleAvatar(
+                          radius: 20,
+                          backgroundImage:
+                              AssetImage('assets/images/default_profile.png'),
+                        );
+                      }
+                    }
+                  },
+                ),
+                const SizedBox(width: 10),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      'Hello!',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontFamily: 'Poppins',
+                        fontSize: 17,
+                        fontWeight: FontWeight.normal,
+                      ),
+                    ),
+                    Text(
+                      _userEmail,
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontFamily: 'Poppins',
+                        fontSize: 17,
+                        fontWeight: FontWeight.normal,
+                      ),
+                    ),
+                  ],
+                ),
+                const Spacer(),
+              ],
+            ),
+          ),
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.logout, color: Colors.white),
+              onPressed: _logout,
+              tooltip: 'Logout',
+            ),
+          ],
+        ),
       ),
       body: _pages[_selectedIndex],
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _selectedIndex,
-        onTap: _onItemTapped,
-        items: const [
-          BottomNavigationBarItem(icon: Icon(Icons.task), label: 'Tugas'),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.calendar_today), label: 'Kalender'),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.account_circle), label: 'Akun'),
-        ],
+      bottomNavigationBar: SafeArea(
+        child: Container(
+          margin: const EdgeInsets.all(8.0),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(28),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black12,
+                offset: Offset(0, 3),
+                blurRadius: 8,
+              ),
+            ],
+          ),
+          child: BottomNavigationBar(
+            currentIndex: _selectedIndex,
+            onTap: _onItemTapped,
+            items: [
+              BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.task,
+                  color: _selectedIndex == 0 ? Colors.deepPurple : Colors.grey,
+                ),
+                label: 'Tugas',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.calendar_today,
+                  color: _selectedIndex == 1 ? Colors.deepPurple : Colors.grey,
+                ),
+                label: 'Kalender',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.account_circle,
+                  color: _selectedIndex == 2 ? Colors.deepPurple : Colors.grey,
+                ),
+                label: 'Akun',
+              ),
+            ],
+            backgroundColor: Colors.transparent,
+            elevation: 0,
+            selectedItemColor: Colors.deepPurple,
+            unselectedItemColor: Colors.grey,
+            selectedLabelStyle: const TextStyle(
+              fontFamily: 'Roboto',
+              fontSize: 14,
+            ),
+            unselectedLabelStyle: const TextStyle(
+              fontFamily: 'Roboto',
+              fontSize: 14,
+            ),
+          ),
+        ),
+      ),
+      floatingActionButton: Padding(
+        padding: const EdgeInsets.only(bottom: 2, right: 16),
+        child: FloatingActionButton(
+          backgroundColor: const Color.fromRGBO(107, 33, 168, 1),
+          foregroundColor: Colors.white,
+          onPressed: () async {
+            await _showAddTaskDialog(context);
+          },
+          child: const Icon(Icons.add),
+        ),
       ),
     );
   }
